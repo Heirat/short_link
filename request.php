8a601fa984dc053a90733852dc0025b1bf6fcb07
Heirat <neocron76@gmail.com>
2022-05-09 23:30:20 +0300

    Добавил функцию генерации уникального токена

--- request.php
+++ request.php
@@ -7,28 +7,53 @@ if (!$conn)
     die("Ошибка подключения к базе данных: \n" . mysqli_connect_error());
 
 if (isset($_GET['long_link']))
-    $request = $conn->real_escape_string(trim($_GET['long_link']));
+    $long_link = $conn->real_escape_string(trim($_GET['long_link']));
 
-if (!empty($request)) {
+if (!empty($long_link)) {
+    $sel = $conn->query("SELECT * FROM links WHERE link = '" . $long_link . "'");
 
-    $sel = $conn->query("SELECT * FROM links WHERE link = '" . $request . "'");
-
+    // Одна ссылка - один токен
+    // Если этой ссылки нет в БД
     if ($sel->num_rows == 0) {
-        $token = token_gen();
+        $canInsert = false;
+        $token = generate_unique_token($conn);
 
-        $ins = $conn->query("INSERT INTO links (link, token) VALUES ('" . $request . "', '" . $token . "')");
+
+        $ins = $conn->query("INSERT INTO links (link, token) VALUES ('" . $long_link . "', '" . $token . "')");
 
         if ($ins) {
             echo 'Добавлена ссылка';
-        }
-        else {
+        } else {
             echo 'Ссылка не добавлена';
         }
     }
     else {
-        echo 'Эта ссылка уже есть в базе данных';
+        echo 'Эта ссылка уже добавлена в БД. Отобразить';
     }
 
+}
+
+/**
+ * Генерирует токен и делает запрос к БД, повторяя, пока не получит уникальный токен.
+ * @param mysqli $connection Дескриптор соеднинения с БД
+ * @param int $min Минимальная длина токена
+ * @param int $max Максимальная длина токена
+ * @return string Токен
+ */
+function generate_unique_token(mysqli $connection, int $min = 5, int $max = 8): string {
+    $token = '';
+    while (true) {
+        $token = generate_token($min, $max);
+        // Получаем этот токен из БД
+        $select = $connection->query("SELECT * FROM links WHERE token = '" . $token . "'");
+
+        // Если получен токен, которого нет в БД
+        if ($select->num_rows == 0) {
+            break;
+        }
+    }
+
+    return $token;
 }
 
 /**
@@ -38,7 +63,7 @@ if (!empty($request)) {
  * @param int $max Максимальная длина токена
  * @return string Токен
  */
-function token_gen(int $min = 5, int $max = 8): string
+function generate_token(int $min = 5, int $max = 8): string
 {
     $chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
 
