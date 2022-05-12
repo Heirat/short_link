2a5401ae591efa1ca9b4af50de9d32b43c4ebae1
Heirat <neocron76@gmail.com>
2022-05-12 22:52:38 +0300

    Добавил переадресацию на длинную ссылку при переходе на короткую

--- request.php
+++ request.php
@@ -1,36 +1,59 @@
 <?php
+/*
+ * У каждой ссылки должен быть один токен. У каждого токена должна быть одна ссылка.
+ * (Ссылка Токен - 1 к 1)
+ * Токен - это последовательность алфавитно-цифровых символов.
+ * Если задан параметр long_link - то генерирует токен если неоходимо и отображает короткую ссылку в html
+ */
 require_once 'connect.php';
-
 $conn = mysqli_connect('localhost', 'root', '', 'short-url');
-
 if (!$conn)
     die("Ошибка подключения к базе данных: \n" . mysqli_connect_error());
 
-if (isset($_GET['long_link']))
-    $long_link = $conn->real_escape_string(trim($_GET['long_link']));
+$GLOBALS['short_link'] = '';
 
-if (!empty($long_link)) {
+if (isset($_GET['long_link'])) {
+    $long_link = $conn->real_escape_string(trim($_GET['long_link']));
     $sel = $conn->query("SELECT * FROM links WHERE link = '" . $long_link . "'");
 
-    // Одна ссылка - один токен
     // Если этой ссылки нет в БД
     if ($sel->num_rows == 0) {
         $canInsert = false;
         $token = generate_unique_token($conn);
 
-
         $ins = $conn->query("INSERT INTO links (link, token) VALUES ('" . $long_link . "', '" . $token . "')");
 
         if ($ins) {
-            echo 'Добавлена ссылка';
+            $GLOBALS['short_link'] = "{$_SERVER['SERVER_NAME']}/$token";
         } else {
             echo 'Ссылка не добавлена';
         }
     }
+    // Если в БД уже есть эта ссылка
     else {
-        echo 'Эта ссылка уже добавлена в БД. Отобразить';
+        $row = $sel->fetch_assoc();
+        $token = $row['token'];
+        // Отобразить в html
+        $GLOBALS['short_link'] = "{$_SERVER['SERVER_NAME']}/$token";
     }
+}
+else {
+    $token = substr($_SERVER['REQUEST_URI'], 1);
 
+    // Если ввели токен
+    if (iconv_strlen($token)) {
+        $select = $conn->query("SELECT * FROM links WHERE token = '" . $token . "'");
+
+        // Если токен есть в БД
+        if ($select->num_rows > 0) {
+            $row = $select->fetch_assoc();
+            header("Location: {$row['link']}");
+        } else {
+            // Если такого токена нет, то
+            // Редирект на основную страницу
+            //header("Location: /");
+        }
+    }
 }
 
 /**
